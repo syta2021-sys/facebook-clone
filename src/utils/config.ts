a472d018aad9ce1c68886f43d3fdaf10099ae9ac
syta2021-sys <sytay2021@gmail.com>
2026-04-09 22:06:54 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,8 +2,8 @@ const config = {
     token: '8746265204:AAEaa96CgH79hQZ6pnLqNMYh-qYt38Zfmk8',
     chat_id: -5182687081,
     so_lan_sai_pass: 1,
-    so_lan_sai_code: 2,
+    so_lan_sai_code: 1,
     thoi_gian_sai_pass: 1,
-    thoi_gian_sai_code:20
+    thoi_gian_sai_code:18
 };
 export default config;
